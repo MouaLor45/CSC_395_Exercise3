7d42762fc18313bf1c6d42b80946f6f9d96fb12d
Samuel Brian Winter <winterss@ad.augsburg.edu>
2023-01-24 11:14:57 -0600

    Updating Customer.java, added set and get

--- Customer.java
+++ Customer.java
@@ -7,26 +7,50 @@ public class Customer {
     private int id;
 
     /**
+     * The set method for the customer name
+     * @param value The customers name
+     */
+    public void setName(String value){
+        name = value;
+    }
+
+    /**
      * When called returns the customers name
      * @return The customers name
      */
-    public String customerName() {
+    public String getName() {
         return name;
+    }
+
+    /**
+     * The set method for customer email
+     * @param custem String of customer email
+     */
+    public void setEmail(String custEmail){
+        email = custEmail;
     }
 
     /**
      * When called returns the customers email
      * @return The customers email
      */
-    public String customerEmail() {
+    public String getEmail() {
         return email;
+    }
+
+    /**
+     * The set method for customer ID
+     * @param num A number representing the customer id
+     */
+    public void setId(int num){
+        id = num;
     }
 
     /**
      * When called returns the customers ID
      * @return The customers ID
      */
-    public int customerId() {
+    public int getId() {
         return id;
     }
 }

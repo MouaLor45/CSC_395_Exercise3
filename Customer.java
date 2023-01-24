4ba4e1f6e57d73abc4ff7e149770ededbb53de68
Samuel Brian Winter <winterss@ad.augsburg.edu>
2023-01-24 10:58:58 -0600

    Adding fixed customer code

---
+++ Customer.java
@@ -0,0 +1,33 @@
+/**
+ * Customer Class
+ */
+public class Customer {
+    private String name;
+    private String email;
+    private int id;
+
+    /**
+     * When called returns the customers name
+     * @return The customers name
+     */
+    public String customerName() {
+        return name;
+    }
+
+    /**
+     * When called returns the customers email
+     * @return The customers email
+     */
+    public String customerEmail() {
+        return email;
+    }
+
+    /**
+     * When called returns the customers ID
+     * @return The customers ID
+     */
+    public int customerId() {
+        return id;
+    }
+}
+   
